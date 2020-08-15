d64a5c9db5dda6da4da2feaf6662b54467685324
Javi <xgc1986@gmail.com>
2020-08-15 14:11:20 +0200

    minor fix

--- src/Exercises/IO/Controller/DefaultController.php
+++ src/Exercises/IO/Controller/DefaultController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Exercises\IO\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,7 +18,7 @@ class DefaultController extends AbstractController
     /**
      * @Route("/dots")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $response = $this->render('exercise/dots.html.twig');
         $content = $response->getContent();
