ee96b8ced5fa6a141dbc09352e230a58b262668d
Javier Gonzalez <xgc1986@gmail.com>
2020-08-17 12:33:12 +0200

    add initial cv (#10)

--- src/CV/IO/Controller/DefaultController.php
+++ src/CV/IO/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Exercises\IO\Controller;
+namespace App\CV\IO\Controller;
 
 use App\Common\Controller\EtagCacheabkeController;
 use App\Common\Controller\WebController;
@@ -10,16 +10,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/exercise")
+ * @Route("/")
  */
 class DefaultController extends WebController implements EtagCacheabkeController
 {
 
     /**
-     * @Route("/dots")
+     * @Route("")
      */
     public function index(): Response
     {
-        return $this->render('dots.html.twig');
+        return $this->render('cv.html.twig');
     }
 }
